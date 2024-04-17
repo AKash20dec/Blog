e8ae3b9d71ed37f80e6f4c27cba1a2f575822985
AKash20dec <yakash20dec@gmail.com>
2024-04-17 22:27:33 +0530

    authentication and frontend

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const PORT = 8000;
+const authRoutes =require('./Routes/Auth');
 require('dotenv').config();
 require('./db')
+const User =require('./Models/UserSchema');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/auth',authRoutes);
 
 app.get("/", (req, res) => {
     res.json({message:"the API is working"});
